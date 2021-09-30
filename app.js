b6dbf55d35701a46e04eb85089b1cd2813a300fc
HZ-2000 <delta-z@outlook.com>
2021-09-30 13:10:31 -0700

    Added a navbar and linked a login page and a register page, both empty

--- app.js
+++ app.js
@@ -13,4 +13,12 @@ app.get('', (req, res) => {
   res.render("index")
 })
 
+app.get('/login', (req, res) => {
+  res.render("login")
+})
+
+app.get('/register', (req, res) => {
+  res.render("register")
+})
+
 app.listen(port, () => {console.log(`Listening on Port: ${port}`)})
