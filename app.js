a3a783e4d072df730688c8c46897b224e778b72a
HZ-2000 <delta-z@outlook.com>
2021-10-16 16:31:42 -0700

    Added login functionality!!!

--- app.js
+++ app.js
@@ -1,28 +1,113 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
+const bcrypt = require('bcrypt')
+const initializePassport = require('./public/js/login.js')
 const app = express()
 const port = 3000
+const login_succesful = false;
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+//temporary
+const users = []
+
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
 //static files
 app.use(express.static('public'))
 app.use(express.json())
 app.use("/css", express.static(__dirname + 'public/css'))
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set('view engine', 'ejs')
 
-app.get('', (req, res) => {
+//------------general site---------------------
+
+app.get('/', checkNotAuthenticated, (req, res) => {
   res.render("index")
 })
 
-app.get('/login', (req, res) => {
+app.get('/about',checkNotAuthenticated, (req, res) => {
+  res.render("about")
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render("login")
 })
 
-app.get('/register', (req, res) => {
-  res.render("register")
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/services',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render("register",{login_succesful})
 })
 
-app.get('/services', (req, res) => {
-  res.render("services")
+app.post('/register', async(req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    //---------insert data base------------
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+  console.log(users)
 })
+
+//------------Main user hub-----------------
+
+app.get('/services', checkAuthenticated, (req, res) => {
+  res.render("services",{name: req.user.name})
+})
+
+app.get('/settings', checkAuthenticated, (req, res) => {
+  res.render("settings",{name: req.user.name})
+})
+
+app.get('/complaints', checkAuthenticated, (req, res) => {
+  res.render("complaints",{name: req.user.name})
+})
+
+//---------authentication checks--------------
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/services')
+  }
+
+  next()
+}
+
 
 app.listen(port, () => {console.log(`Listening on Port: ${port}`)})
